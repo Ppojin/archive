de7a0c897ac6bcacf4514b55f6b937cd2276768d
HyojinHwang <hhjin1814@gmail.com>
2023-10-05 17:52:31 +0900

    solve 229

--- leetcodeJava/src/main/java/com/ppojin/leetcode/daily/m2310/Solution.java
+++ leetcodeJava/src/main/java/com/ppojin/leetcode/daily/m2310/Solution.java
@@ -1,11 +1,75 @@
 package com.ppojin.leetcode.daily.m2310;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
+
+    /*
+    Day 4
+    229. Majority Element II
+    */
+    public List<Integer> majorityElement(int[] nums) {
+        // hint: length of result could not bigger then 2
+        int n0 = 0x7fffffff;
+        int n1 = 0x80000000;
+        int c0 = 0;
+        int c1 = 0;
+
+        for (int n : nums) {
+            if (n0 == n) c0++;
+            else if (n1 == n) c1++;
+            else if (c0 == 0) {
+                n0 = n;
+                c0 = 1;
+            } else if (c1 == 0) {
+                n1 = n;
+                c1 = 1;
+            } else {
+                c0--;
+                c1--;
+            }
+        }
+
+        c0 = 0;
+        c1 = 0;
+        for (int n: nums){
+            if(n0 == n) c0++;
+            if(n1 == n) c1++;
+        }
+        System.out.printf("%d %d", c0, c1);
+
+        if (c1 > nums.length / 3 && c0 > nums.length / 3){
+            return List.of(n0, n1);
+        } else if(c0 > nums.length / 3){
+            return List.of(n0);
+        } else if(c1 > nums.length / 3){
+            return List.of(n1);
+        } else {
+            return List.of();
+        }
+
+//        List<Integer> result = new ArrayList<>();
+//        int[] array = Arrays.stream(nums).sorted().toArray();
+//        int past = array[0];
+//        int count = 1;
+//        for (int i = 1; i < nums.length; i++) {
+//            if (past == array[i]) {
+//                count += 1;
+//            } else {
+//                if (count > less) {
+//                    result.add(past);
+//                }
+//                past = array[i];
+//                count = 1;
+//            }
+//        }
+//        if (count > less && !result.contains(past)) {
+//            result.add(past);
+//        }
+//        return result;
+    }
 
     /*
     Day 3
